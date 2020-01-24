9eed33e3dcbdb6b39c44a7f24c07da757ccb8638
BASIC\suyama <daichi.suyama@basic.co.jp>
2020-01-24 15:52:00 +0900

    検索

--- TrialAzureSearch/Controllers/TrialAzureSearchController.cs
+++ TrialAzureSearch/Controllers/TrialAzureSearchController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using TrialAzureSearch.Models;
@@ -23,27 +24,41 @@ namespace TrialAzureSearch.Controllers
         private readonly ILogger<TrialAzureSearchController> _logger;
         private Azuresearchconfig _azureSearchConfig = null;
         private SearchServiceClient _serviceClient = null;
+        private ISearchIndexClient _indexClient;
 
         public TrialAzureSearchController(ILogger<TrialAzureSearchController> logger, IOptions<Azuresearchconfig> config)
         {
             _azureSearchConfig = config.Value;
             _serviceClient = new SearchServiceClient(_azureSearchConfig.SearchServiceName, new SearchCredentials(_azureSearchConfig.SearchServiceAdminApiKey));
+            _indexClient = _serviceClient.Indexes.GetClient(_azureSearchConfig.SearchIndexName);
             _logger = logger;
         }
 
         [Route("search")]
-        [HttpGet]
-        public async Task<IEnumerable<WeatherForecast>> AzureSearch()
+        [HttpPost]
+        public async Task<ActionResult> AzureSearch(SearchData model)
         {
-
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            if (string.IsNullOrEmpty(model.searchText))
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                model.searchText = string.Empty;
+            }
+
+            return await RunQueryAsync(model);
+        }
+
+        private async Task<ActionResult> RunQueryAsync(SearchData model)
+        {
+            var parameters = new SearchParameters
+            {
+                // Enter Hotel property names into this list so only these values will be returned.
+                // If Select is empty, all values will be returned, which can be inefficient.
+                Select = new[] { "HotelName", "Description" }
+            };
+
+            // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
+            model.resultList = await _indexClient.Documents.SearchAsync<Hotel>(model.searchText, parameters);
+
+            return Ok(model.resultList);
         }
     }
 }
